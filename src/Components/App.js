a3c6650c19ed3bc3a975288a04b82f98fb3b2805
Logan Vincent <ldvsimp@gmail.com>
2021-12-01 17:28:43 -0700

    Add folders and remove react boilerplate files

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Map from './Map'
+import '../Styles/App.css'
+
+const App = () => {
+
+
+  return (
+    <div className="app">
+      <header className="app-header">Header for Pew Pew Pew App</header>
+    </div>
+  )
+}
+
+export default App
