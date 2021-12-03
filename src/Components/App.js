d111c5ad38cfe3079007abc364c0e532f1d76dd7
Joshua Horner <lesliewbusiness@gmail.com>
2021-12-03 10:39:20 -0700

    Display tile grid

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Map from './Map'
+import Tile from './Tile'
+// import Player from './Player'
 import '../Styles/App.css'
 
 const App = () => {
 
-
   return (
     <div className="app">
       <header className="app-header">Header for Pew Pew Pew App</header>
+        <Map />
     </div>
   )
 }
---
+++ src/Components/Player.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Map from './Map.js';
+
+const Player = () => {
+  window.addEventListener('keydown', (event) => {
+    console.log('Heyooo')
+  })
+};
+
+export default Player;
